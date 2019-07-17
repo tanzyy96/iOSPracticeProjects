70af2919d3fc72de97bf7055c484d66af2d1094a
tanzy <tan.zy096@gmail.com>
2019-07-18 00:04:07 +0800

    Initialise NamesToFaces project

---
+++ NamesToFaces/NamesToFaces/PersonCell.swift
@@ -0,0 +1,14 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Tan Zhi Yang on 17/7/19.
+//  Copyright © 2019 Tan Zhi Yang. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+}
---
+++ NamesToFaces/NamesToFaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Tan Zhi Yang on 17/7/19.
+//  Copyright © 2019 Tan Zhi Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
